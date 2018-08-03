8a6f71717f0f0aa41f98bedcd73e260d9054ab8c
RenanDC <renan.dcardoso@gmail.com>
2018-08-03 15:55:09 -0300

    Adicionado controller quiz

---
+++ controllers/Quiz.py
@@ -0,0 +1,34 @@
+from model.Users import db
+from model.Users import Quiz as quizDB
+
+class Quiz():
+    
+    def findAll(self):
+        return quizDB.query.all()
+
+    def findOne(self,id):
+        return quizDB.query.get(id)
+
+    def insertQuiz(self, info):
+        quiz = quizDB()
+        quiz.name = info['name']
+        quiz.responsetime = info['response_time']
+        quiz.category = info['category']
+        quiz.dificulty = info['dificulty']
+        db.session.add(quiz)
+        return db.session.commit()
+
+    
+    def updateQuiz(self, id, info):
+        quiz = quizDB.query.get(id)
+        print(quiz.id)
+        quiz.name = info['name']
+        quiz.responsetime = info['response_time']
+        quiz.dificulty = info['dificulty']
+        return db.session.commit()
+
+
+    def removeQuiz(self, id):
+        quiz = quizDB.query.get(id)
+        db.session.delete(quiz)
+        return db.session.commit()
