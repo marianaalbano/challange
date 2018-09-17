5bf3a4579a1c572e212376a70f842e982530ff5f
Mariana Albano <malbano@br0fvfwm12bhv2g.local>
2018-09-17 16:53:11 -0300

    questions ok

--- routes/user.py
+++ routes/user.py
@@ -1,10 +1,13 @@
 from flask import Blueprint, render_template, request, redirect, url_for
-from flask_login import LoginManager, login_required, login_user, logout_user
+from flask_login import LoginManager, login_required, login_user, logout_user, current_user
 
 from admin_required import admin_required
 from controllers.Users import User
 from controllers.QuizController import QuizController
 from model.Users import db
+from model.UserResponse import UserResponse
+
+from model.UserResponse import UserResponse
 
 user = Blueprint('user', __name__)
 
@@ -26,10 +29,32 @@ def user_quizzes(id):
     quizzes = quizzes.findUserQuiz(id)
     return render_template("user/quizzes/quizList.html", quizzes=quizzes)
 
-
 @user.route("/user/quiz/<id_quiz>/questions", methods=["GET", "POST"])
 @login_required
 def user_response(id_quiz):
-    questions = QuizController()
-    questions = questions.findOne(id_quiz)
-    return render_template("user/quizzes/quizPlay.html", questions=questions)
+    if request.method == "POST":
+        user_response = UserResponse()
+        print(request.form)
+        response = []
+        for responses in request.form:
+            if "disserty" in responses:
+                response.append({"type":"disserty", 
+                                 "id_question":responses[-1], 
+                                 "response":request.form[responses]})
+            else:
+                response.append({"type":"multiple", 
+                                 "id_question":responses[-1], 
+                                 "response":request.form[responses]})
+                
+                
+        user_response.name = current_user.name
+        user_response.id_user = "%s" %current_user.id
+        user_response.id_quiz = '1'
+        user_response.questions = response
+        user_response.save()
+
+        return redirect('/user/%s/quizzes' %current_user.id)
+    else:
+        questions = QuizController()
+        questions = questions.findOne(id_quiz)
+        return render_template("user/quizzes/quizPlay.html", questions=questions)
